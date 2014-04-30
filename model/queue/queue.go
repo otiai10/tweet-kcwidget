827a83d6932ecfeed9663a12578f8eb271aa890b
otiai10 <otiai10@gmail.com>
2014-04-30 22:03:49 +0900

    Do fmt

--- model/queue/queue.go
+++ model/queue/queue.go
@@ -5,35 +5,35 @@ import "tweet-kcwidget/model/tweet"
 import "strconv"
 
 type Queue struct {
-    Time int64// Unix Time to fire
-    DurationToSleep time.Duration
-    Tweet tweet.Tweet
+	Time            int64 // Unix Time to fire
+	DurationToSleep time.Duration
+	Tweet           tweet.Tweet
 }
 
 var Ch = make(chan tweet.Tweet)
 
 func New(finish_time int64, tweetVal tweet.Tweet) Queue {
-    return Queue{
-        Time: finish_time,
-        DurationToSleep: getDurationFromUnix(finish_time),
-        Tweet: tweetVal,
-    }
+	return Queue{
+		Time:            finish_time,
+		DurationToSleep: getDurationFromUnix(finish_time),
+		Tweet:           tweetVal,
+	}
 }
 
 func getDurationFromUnix(finish_time int64) (dur time.Duration) {
 
-    now := time.Now().Unix()
-    diff := int(finish_time - now - 60)
-    if diff < 0 {
-        return
-    }
-    dur, _ = time.ParseDuration(strconv.Itoa(diff) + "s")
-    return
+	now := time.Now().Unix()
+	diff := int(finish_time - now - 60)
+	if diff < 0 {
+		return
+	}
+	dur, _ = time.ParseDuration(strconv.Itoa(diff) + "s")
+	return
 }
 
 func Enq(q Queue) {
-    go func(){
-        time.Sleep(q.DurationToSleep)
-        Ch <- q.Tweet
-    }()
+	go func() {
+		time.Sleep(q.DurationToSleep)
+		Ch <- q.Tweet
+	}()
 }
--- model/tweet/tweet.go
+++ model/tweet/tweet.go
@@ -4,21 +4,22 @@ import "fmt"
 import "time"
 
 type Tweet struct {
-    ToScreenName string
-    Message string
-    Kind int
+	ToScreenName string
+	Message      string
+	Kind         int
 }
+
 var tweet_format = "@%s %s %s"
 
 func New(screen_name, message string, kind int) Tweet {
-    return Tweet{
-        ToScreenName: screen_name,
-        Message: message,
-        Kind: kind,
-    }
+	return Tweet{
+		ToScreenName: screen_name,
+		Message:      message,
+		Kind:         kind,
+	}
 }
 
 func (t Tweet) ToText() string {
-    debug_suffix := fmt.Sprintf("%v", time.Now().Unix())
-    return fmt.Sprintf(tweet_format, t.ToScreenName, t.Message, debug_suffix)
+	debug_suffix := fmt.Sprintf("%v", time.Now().Unix())
+	return fmt.Sprintf(tweet_format, t.ToScreenName, t.Message, debug_suffix)
 }
