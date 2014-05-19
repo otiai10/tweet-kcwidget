d669bb6d3360ee61a143566b4e041a7f758921b0
otiai10 <otiai10@gmail.com>
2014-05-19 20:48:48 +0900

    Refactor view

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -1,11 +1,13 @@
 package controllers
 
 import "github.com/revel/revel"
+import "tweet-kcwidget/conf/my"
 
 type App struct {
 	*revel.Controller
 }
 
 func (c App) Index() revel.Result {
-	return c.Render()
+	bots := my.BotTokens
+	return c.Render(bots)
 }
