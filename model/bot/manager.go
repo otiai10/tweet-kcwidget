e0d111e6339578c28665ae1a13133abc99752251
otiai10 <otiai10@gmail.com>
2014-05-20 01:00:44 +0900

    Except timed girl from assignees

--- model/bot/manager.go
+++ model/bot/manager.go
@@ -9,15 +9,32 @@ func GetByName(name string) Bot {
 	theToken := my.BotTokens[name]
 	return Bot{name, theToken}
 }
-func GetAssigned(tw tweet.Tweet) Bot {
-	candidates := roster[tw.Kind]
+func getTimeKey(candidates map[int]string) int {
 	hour := time.Now().Hour()
 	key := hour - (hour % (24 / len(candidates)))
-	return GetByName(candidates[key])
+	return key
+}
+func GetAssigned(tw tweet.Tweet) Bot {
+	candidates := getCandidates(tw)
+	return GetByName(pickUpRandom(candidates))
 }
 func GetRandom() Bot {
 	name := pickUpRandom(getAllNames())
 	return GetByName(name)
+}
+func getCandidates(tw tweet.Tweet) (names []string) {
+	_ref := roster[tw.Kind]
+	_except := getTimeKey(_ref)
+	names = make([]string, len(_ref)-1)
+	i := 0
+	for key, name := range _ref {
+		if key == _except {
+			continue
+		}
+		names[i] = name
+		i++
+	}
+	return
 }
 func getAllNames() (names []string) {
 	names = make([]string, len(my.BotTokens))
