b69bfdc0572f44c37585f1a7793bab5a10bbcbb4
otiai10 <otiai10@gmail.com>
2014-05-19 21:52:48 +0900

    Throttle random tweet

--- model/bot/monologue.go
+++ model/bot/monologue.go
@@ -1,6 +1,8 @@
 package bot
 
 import "tweet-kcwidget/model/tweet"
+import "math/rand"
+import "time"
 
 func getMonologueTweetByName(name string) tweet.Tweet {
 	return tweet.New(
@@ -10,6 +12,12 @@ func getMonologueTweetByName(name string) tweet.Tweet {
 	)
 }
 func (b Bot) Monologue() (e error) {
+	// とりあえずランダムで5分の1くらいにしぼる
+	// TODO: ちゃんとする
+	rand.Seed(time.Now().Unix())
+	if rnd := rand.Intn(100); rnd < 80 {
+		return
+	}
 	tw := getMonologueTweetByName(b.Name)
 	return b.Tweet(tw)
 }
